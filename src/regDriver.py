3b6667a254db7792db8bfc6a833a756dc869ba81
husen <husensofteng@gmail.com>
2017-11-03 09:03:10 +0100

    f

--- src/regDriver.py
+++ src/regDriver.py
@@ -346,6 +346,7 @@ if __name__ == '__main__':
             all_dfs = pd.concat(dfs)
             fig = plt.figure(figsize=(12,8))
             s = sns.stripplot(x='Tissue', y='Frequency', data=all_dfs, jitter=True)
+            s.set_ylim([0,80000])
             ss = s.get_figure()
             ss.savefig('fig4.pdf', bbox_inches='tight')
             ss.savefig('fig4.svg', bbox_inches='tight')
