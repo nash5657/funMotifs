1b6a28f0709d086936dcad2c70749efe8f0ea472
husen <husensofteng@gmail.com>
2017-11-17 11:43:13 +0100

    run regDriver in parallel when a dir is given instead of a file (-dir option is added)

--- src/regDriver.py
+++ src/regDriver.py
@@ -3,31 +3,19 @@ Created on 21 Oct 2017
 
 @author: husensofteng
 '''
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib.pyplot import tight_layout
-import matplotlib.pyplot as plt
-
 import sys, os
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import string
-import psycopg2
 from multiprocessing import Pool
+import psycopg2
 from psycopg2.extras import DictCursor
 import time
-#plt.style.use('ggplot')
-sns.set_style("white")
-#sns.set_context("paper")#talk
-#plt.style.use('seaborn-ticks')
 
-params = {'-sep': '\t', '-cols_to_retrieve':'chr, motifstart, motifend, strand, name, score, pval, fscore, chromhmm, contactingdomain, dnase__seq, fantom, loopdomain, numothertfbinding, othertfbinding, replidomain, tfbinding, tfexpr', '-number_rows_select':'all',
+params = {'-sep': '\t', 
+          '-cols_to_retrieve':'chr, motifstart, motifend, strand, name, score, pval, fscore, chromhmm, contactingdomain, dnase__seq, fantom, loopdomain, numothertfbinding, othertfbinding, replidomain, tfbinding, tfexpr', '-number_rows_select':'all',
           '-restart_conn_after_n_queries':100000, '-variants':True, '-regions':True,
           '-chr':0, '-start':1, '-end':2, '-ref':3, '-alt':4, 
           '-db_name':'regmotifsdbtest', '-db_host':'localhost', '-db_port':5432, '-db_user':'huum', '-db_password':'',
           '-all_motifs':True, '-motifs_tfbining':False, '-max_score_motif':False, '-motifs_tfbinding_otherwise_max_score_motif':False,
-          '-verbose': True}
+          '-verbose': True, '-num_cores':1}
     
 def get_params(params_list, params_without_value):
     global params
@@ -87,12 +75,12 @@ def run_query_nocursorname(cols_to_retrieve, from_tabes, cond_statement, curs):
         #curs.close()
         return []
     
-def read_infile():
+def read_infile(infile):
     conn = open_connection()
     curs_for_pfms = conn.cursor()
     number_lines_processed = 0
     t = time.time()
-    with open(params['-f'], 'r') as infile, open(params['-f']+'_annotated.tsv', 'w') as outfile:
+    with open(infile, 'r') as infile, open(infile+'_annotated.tsv', 'w') as outfile:
         line = infile.readline()
         cols_from_file = ['cols'+str(i) for i in range(0,len(line.strip().split(params['-sep'])))]
         cols_from_file.extend((params['-cols_to_retrieve'] + ',mutposition,entropy').split(','))
@@ -192,7 +180,7 @@ def read_infile():
             
             number_lines_processed+=1
             if number_lines_processed % int(params['-restart_conn_after_n_queries']) == 0:
-                print '{} Lines are processed from {}'.format(number_lines_processed, params['-f'])
+                print '{} Lines are processed from {}'.format(number_lines_processed, infile)
                 print time.time()-t
                 t = time.time()
                 conn.close()
@@ -201,141 +189,24 @@ def read_infile():
                 curs_for_pfms = conn.cursor()
     return number_lines_processed    
 
-def plot_motif_freq(tf_name, tissue_table, motifs_table, min_fscore):
+def run_regDriver(args):
+    if len(args)<=0:
+        print "Usage: python regDriver.py -f input_file -tissue tissue_name [options]"
+        sys.exit(0)
+    get_params(args, params_without_value=[])
     
-    conn = open_connection()
-    curs = conn.cursor()#cursor_factory=DictCursor)
-    
-    stmt_all = "select count({tissue}.mid) from {motifs},{tissue} where {motifs}.mid={tissue}.mid and {motifs}.name like '%{tf_name}%'".format(motifs=motifs_table, tissue=tissue_table, tf_name=tf_name)
-    print stmt_all
-    stmt_tfbinding = "select count({tissue}.mid) from {motifs},{tissue} where {motifs}.mid={tissue}.mid and {motifs}.name like '%{tf_name}%' and ({tissue}.tfbinding>0 and {tissue}.tfbinding!='NaN')".format(motifs=motifs_table, tissue=tissue_table,tf_name=tf_name)
-    stmt_dnase = "select count({tissue}.mid) from {motifs},{tissue} where {motifs}.mid={tissue}.mid and {motifs}.name like '%{tf_name}%' and ({tissue}.dnase__seq>0 and {tissue}.dnase__seq!='NaN')".format(motifs=motifs_table, tissue=tissue_table, tf_name=tf_name)
-    stmt_active = "select count({tissue}.mid) from {motifs},{tissue} where {motifs}.mid={tissue}.mid and {motifs}.name like '%{tf_name}%' and ((fscore>{min_fscore} and dnase__seq>0 and dnase__seq!='NaN') or (tfbinding>0 and {tissue}.tfbinding!='NaN'))".format(motifs=motifs_table, tissue=tissue_table, tf_name=tf_name, min_fscore=min_fscore)
-    curs.execute(stmt_all)
-    motifs_all = curs.fetchall()
-    curs.execute(stmt_tfbinding)
-    tfbinding = curs.fetchall()
-    curs.execute(stmt_dnase)
-    dnase = curs.fetchall()
-    curs.execute(stmt_active)
-    active = curs.fetchall()
-    curs.close()
-    return [[tf_name, tissue_table, 'All Motifs', int(motifs_all[0][0])], 
-            [tf_name, tissue_table, 'DHSs', int(dnase[0][0])], 
-            [tf_name, tissue_table, 'Matching TFBSs', int(tfbinding[0][0])], 
-            [tf_name, tissue_table, 'Active Motifs', int(active[0][0])]]
-
-def plot_fscore(tf_name, tissue_table, motifs_table, tissue_names, fig_name):
-    
-    conn = open_connection()
-    curs = conn.cursor()#cursor_factory=DictCursor)
-    
-    stmt_all = "select {tissue_names} from {motifs},{tissue} where {motifs}.mid={tissue}.mid and {motifs}.name like '%{tf_name}%'".format(
-        tissue_names=','.join(sorted(tissue_names)), motifs=motifs_table, tissue=tissue_table, tf_name=tf_name)
-    print stmt_all
-    curs.execute(stmt_all)
-    scores_all = curs.fetchall()
-    curs.close()
-    df = pd.DataFrame(scores_all, columns=tissue_names)
-    s = sns.boxplot(data=df, color='grey')
-    ss = s.get_figure()
-    ss.savefig(fig_name+'.pdf', bbox_inches='tight')
-    ss.savefig(fig_name+'.svg', bbox_inches='tight')
-    return
-
-def plot_heatmap(min_fscore, motifs_table,tissue_table, fig_name, threshold_to_include_tf):
-    conn = open_connection()
-    curs = conn.cursor()#cursor_factory=DictCursor)
-    
-    stmt_all = "select chromhmm, upper(split_part(name,'_', 1)), count(name) from {motifs},{tissue} where {motifs}.mid={tissue}.mid and ({tissue}.fscore>{min_fscore} or (tfbinding>0 and tfbinding!='NaN')) group by chromhmm,name order by chromhmm".format(
-        motifs=motifs_table, tissue=tissue_table, min_fscore=min_fscore)
-    print stmt_all
-    curs.execute(stmt_all)
-    scores_all = curs.fetchall()
-    curs.close()
-    df = pd.DataFrame(scores_all, columns=['Chromatin States', 'TFs', 'Frequency'])
-    df_pivot = df.pivot('Chromatin States', 'TFs', 'Frequency')
-    df_pivot_filtered = pd.DataFrame()
-    for c in df_pivot.columns:
-            if df_pivot[c].sum()>threshold_to_include_tf:
-                df_pivot_filtered[c] = df_pivot[c] 
-    print df_pivot_filtered.head()
-    if len(df_pivot_filtered)>0:
-        s = sns.heatmap(data=df_pivot_filtered, square=True, cbar=True)
-        ss = s.get_figure()
-        ss.savefig(fig_name+'.pdf', bbox_inches='tight')
-        ss.savefig(fig_name+'.svg', bbox_inches='tight')
-    
-def plot_scatter_plot(min_fscore, motifs_table, tissue_table):
-    conn = open_connection()
-    curs = conn.cursor()#cursor_factory=DictCursor)
-    
-    stmt_all = "select upper(split_part(name,'_', 1)), count(name) as freq from {motifs},{tissue} where {motifs}.mid={tissue}.mid and ({tissue}.fscore>{min_fscore} or (tfbinding>0 and tfbinding!='NaN')) group by name order by freq desc".format(
-        motifs=motifs_table, tissue=tissue_table, min_fscore=min_fscore)
-    print stmt_all
-    curs.execute(stmt_all)
-    scores_all = curs.fetchall()
-    curs.close()
-    df = pd.DataFrame(scores_all, columns=['TFs', 'Frequency'])
-    df['Tissue']=[tissue_table for i in range(0,len(df))]
-    return df
-
+    if '-f' in params.keys():
+        read_infile(params['-f'])
+    elif '-dir' in params.keys():
+        p = Pool(int(params['-num_cores']))
+        for f in os.listdir(params['-dir']):
+            p.apply_async(read_infile, args= (params['-dir'].strip()+'/'+f))
+        p.close()
+        p.join()
 if __name__ == '__main__':
     
-    if len(sys.argv)<=0:
-        print "Usage: python regDriver.py input_file [options]"
-        sys.exit(0)
-    get_params(sys.argv[1:], params_without_value=[])
-    if '-f' in params.keys():
-        try:
-            read_infile()
-        except KeyError:
-            print "No value was found for one or more of the arguments:\n", params
-            print "Usage: python regDriver.py -f file_name -tissue tissue_name"
-    
-    if '-plot' in params.keys():
-        motifs_table='motifs'
-        min_fscore = 2.5
-        tissue_tables=['blood', 'brain', 'breast','cervix', 'colon', 'esophagus', 'kidney', 'liver', 'lung', 'myeloid', 'pancreas', 'prostate', 'skin', 'stomach', 'uterus']
-        tfs = ['CTCF', 'CEBPB', 'FOXA1', 'KFL14', 'HNF4A', 'MAFK']
-        threshold_to_include_tf_in_heatmap = 20000
-        sns.despine(right=True, top=True, bottom=False, left=False)
-        if '-fig1' in params.keys():
-            print 'plotting figure 1'
-            for tissue_table in ['blood', 'liver']:
-                fig = plt.figure()
-                tfs_freq = []
-                for tf in sorted(tfs):
-                    tfs_freq.extend(plot_motif_freq(tf_name=tf, tissue_table = tissue_table, motifs_table = motifs_table, min_fscore = min_fscore))
-                df = pd.DataFrame(tfs_freq, columns = ['TFs', 'Tissue', 'Activity', 'Frequency'])
-                fig = plt.figure()
-                s = sns.barplot(x='TFs', y='Frequency', hue='Activity', data=df, estimator=sum)
-                ss = s.get_figure()
-                ss.savefig('fig1_'+tissue_table+'.pdf', bbox_inches='tight')
-                ss.savefig('fig1_'+tissue_table+'.svg', bbox_inches='tight')
-               
-        if '-fig2' in params.keys():
-            print 'plotting figure 2'
-            for tf in sorted(tfs):
-                fig = plt.figure(figsize=(12,6))
-                plot_fscore(tf_name=tf, tissue_table='all_tissues', motifs_table=motifs_table, tissue_names=tissue_tables, fig_name='fig2_'+tf)
-        
-        if '-fig3' in params.keys():
-            print 'plotting figure 3'
-            for tissue_table in ['liver', 'blood', 'breast']:
-                fig = plt.figure(figsize=(12,6))
-                plot_heatmap(min_fscore = min_fscore, motifs_table=motifs_table,tissue_table=tissue_table, fig_name='fig3_'+tissue_table, threshold_to_include_tf=threshold_to_include_tf_in_heatmap)
-        
-        if '-fig4' in params.keys():
-            print 'plotting figure 4' 
-            dfs = []
-            for tissue_table in tissue_tables:
-                dfs.append(plot_scatter_plot(min_fscore, motifs_table, tissue_table))
-            
-            all_dfs = pd.concat(dfs)
-            fig = plt.figure(figsize=(12,8))
-            s = sns.stripplot(x='Tissue', y='Frequency', data=all_dfs, jitter=True)
-            s.set_ylim([0,80000])
-            ss = s.get_figure()
-            ss.savefig('fig4.pdf', bbox_inches='tight')
-            ss.savefig('fig4.svg', bbox_inches='tight')
+    try:
+        run_regDriver(sys.argv[1:])
+    except KeyError:
+        print "No value was found for one or more of the arguments:\n", params
+        print "Usage: python regDriver.py -f file_name -tissue tissue_name"
