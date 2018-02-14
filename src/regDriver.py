28004018b32caa310f6f0d6d9f378368b808ffae
husen <husensofteng@gmail.com>
2018-02-14 11:48:44 +0100

    error message

--- src/regDriver.py
+++ src/regDriver.py
@@ -60,7 +60,7 @@ def run_query(cols_to_retrieve, from_tabes, cond_statement, order_by_stmt, conn,
         curs.execute(stmt)
     except psycopg2.ProgrammingError:
         conn.rollback()
-        print "error in line number (", n, "):"
+        print "error in line number (", n, ")"
         return []
     if curs is not None:
         return curs.fetchall()
